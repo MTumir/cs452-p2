9330dd5a0fca853dbaef0ceff5cbf033c9ccbf71
Max Tumir <98791648+MTumir@users.noreply.github.com>
2025-02-11 18:18:13 +0000

    lab.c outlined

--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,64 @@
-/**Update this file with the starter code**/
+/**
+ * File: lab.c
+ * Author: MTumir
+ * Created: 02.11.2025
+ * Latest: 02.11.2025
+ * 
+ * Description: Implements functions defined in lab.h.
+ */
+
+#include "lab.h"
+
+char *get_prompt(const char *env) {
+
+    return NULL;
+
+}
+
+int change_dir(char **dir) {
+
+    return -1;
+
+}
+
+char **cmd_parse(char const *line) {
+
+    return NULL;
+
+}
+
+void cmd_free(char ** line) {
+
+
+
+}
+
+char *trim_white(char *line) {
+
+    return NULL;
+
+}
+
+bool do_builtin(struct shell *sh, char **argv) {
+
+    return FALSE;
+
+}
+
+void sh_init(struct shell *sh) {
+
+
+
+}
+
+void sh_destroy(struct shell *sh) {
+
+
+
+}
+
+void parse_args(int argc, char **argv) {
+
+
+    
+}
