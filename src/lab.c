a954b02efaa9f508a6a8878f2ca8ee6bfb7d3cab
Max Tumir <98791648+MTumir@users.noreply.github.com>
2025-03-01 00:37:11 +0000

    implemented code review feedback

--- src/lab.c
+++ src/lab.c
@@ -2,7 +2,7 @@
  * File: lab.c
  * Author: MTumir
  * Created: 02.11.2025
- * Latest: 02.25.2025
+ * Latest: 02.28.2025
  * 
  * Description: Implements functions defined in lab.h.
  */
@@ -18,7 +18,7 @@
 char *get_prompt(const char *env) {
 
     char *prompt = getenv(env);
-    if (!prompt) {
+    if (!prompt) {      // CR- If the env variable doesn't exist, revert to default prompt
         return strdup("shell>");
     } return strdup(prompt);
 
@@ -26,7 +26,8 @@ char *get_prompt(const char *env) {
 
 int change_dir(char **dir) {
 
-    // If no args are provided
+    // CR- If no args are provided, chdir with HOME env variable.
+    //      If this fails, get home directory through user_id.
     if (!dir[1] && !chdir(getenv("HOME"))) {
         uid_t user_id = getuid();
         struct passwd *user_pwd = getpwuid(user_id);
@@ -45,13 +46,28 @@ int change_dir(char **dir) {
 
 }
 
+/**
+ * CR- Prints all previous commands entered into the current shell.
+ */
+void get_history(void) {
+
+    HIST_ENTRY **list = history_list();
+    if (list) {
+        for (int i = 0; list[i]; i ++) {
+            printf ("%d: %s\n", i + history_base, list[i]->line);
+        }
+    }
+
+}
+
 char **cmd_parse(char const *line) {
 
-    char **cmd_array = (char **)malloc(64 * sizeof(char *));
+    // CR- Updated cmd_array allocation to use ARG_MAX, as was shown in class
+    char **cmd_array = (char**) calloc (sysconf(_SC_ARG_MAX), sizeof(char*));
     char *line_copy = strdup(line);
     char *token = strtok(line_copy, " ");
     
-    // Add tokens to cmdArray, delimited by space
+    // Add tokens to cmd_array, delimited by space
     int i = 0;
     while (token) {
         cmd_array[i++] = strdup(token);
@@ -66,9 +82,11 @@ char **cmd_parse(char const *line) {
 
 void cmd_free(char ** line) {
 
+    // CR- Iterate through line, freeing each char *
     for (size_t i = 0; line[i]; i ++) {
         free(line[i]);
     }
+
     free(line);
 
 }
@@ -102,12 +120,7 @@ bool do_builtin(struct shell *sh, char **argv) {
         change_dir(argv);
         return true;
     } if (strcmp(command, "history") == 0) {
-        HIST_ENTRY **list = history_list();
-        if (list) {
-            for (int i = 0; list[i]; i ++) {
-                printf ("%d: %s\n", i + history_base, list[i]->line);
-            }
-        }
+        get_history();      // CR- Moved history code to function
         return true;
     }
 
@@ -120,7 +133,17 @@ void sh_init(struct shell *sh) {
     sh->shell_terminal = STDIN_FILENO;
     sh->shell_is_interactive = isatty(sh->shell_terminal);
     sh->prompt = get_prompt("MY_PROMPT");
-    sh->shell_pgid = getpid();
+    // sh->shell_pgid = getpid();   // CR- Commented unnecessary code
+
+    // CR- Added code important to the shell running?
+    // CR- While the shell is interactive...
+    if(sh->shell_is_interactive){
+        // CR- While the ID of the terminal is not equal to the ID of the shell...
+        while(tcgetpgrp(sh->shell_terminal) != (sh->shell_pgid = getpgrp()))
+            // CR- Stop the shell's process.
+            //      https://www.gnu.org/software/libc/manual/html_node/Job-Control-Signals.html
+            kill(-sh->shell_pgid, SIGTTIN);
+    }
 
     // Signals to ignore
     signal(SIGINT, SIG_IGN);
